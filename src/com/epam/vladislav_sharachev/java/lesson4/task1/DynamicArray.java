84642bd90dead4724f043a8f84d01b258cd825a9
Vlad Sharachev <vladislav_sharachev@epam.com>
2019-08-11 16:58:52 +0400

    les4 vers 1.2b

--- src/com/epam/vladislav_sharachev/java/lesson4/task1/DynamicArray.java
+++ src/com/epam/vladislav_sharachev/java/lesson4/task1/DynamicArray.java
@@ -2,46 +2,41 @@ package com.epam.vladislav_sharachev.java.lesson4.task1;
 
 public class DynamicArray {
     public DynamicArray(int myValue) { //конструктор
-        this.data = new Object[myValue];
+        this.myArray = new Object[myValue];
     }
+    public Object[] myArray;
 
-    public Object[] data;
-    public int size = 0;
-
-    public int size() { //размер массива
-        return size;
-    }
-
-    void add(Object indexToAdd) { //возможность добавления элементов в массив
-        if (data.length <= size) { //если длинна массива меньше содержащихся в нем элементов
-            Object[] tmp = data;
-            data = new String[size];
-            for (int i = 0; i < tmp.length; i++) {
-                data[i] = tmp[i];
+    public int elements = 0; //количество элементов
+    void add(Object elementToAdd) { //возможность добавления элементов в массив
+        if (myArray.length <= elements) { //если длинна массива меньше содержащихся в нем элементов
+            Object[] tempArray = myArray; //создаем 2-й массив
+            myArray = new String[elements];
+            for (int i = 0; i < tempArray.length; i++) {
+                myArray[i] = tempArray[i];
             }
         }
-        data[size] = indexToAdd; // добавляет элемент в массив
-        size++; //добавляет +1 ячейку к размеру массива
+        myArray[elements] = elementToAdd; // добавляет элемент в массив
+        elements++; //добавляет +1 ячейку к размеру массива
     }
 
-    void remove(int toRemove) { //возможность добавления элементов в массив
-        Object[] newArray = data; //равняем старый массив с новым
-        for (int i = 0; i < data.length; i++) {
-            if (i < toRemove) {
-                data[i] = newArray[i];
-            } else if (i > toRemove) {
-                data[i - 1] = newArray[i];
+    void remove(int elementToRemove) { //возможность даления элементов из массива
+        Object[] tempArray = myArray; //равняем старый массив с новым
+        for (int i = 0; i < myArray.length; i++) {
+            if (i < elementToRemove) {
+                myArray[i] = tempArray[i];
+            } else if (i > elementToRemove) {
+                myArray[i - 1] = tempArray[i];
             }
         }
     }
 
-    public Object get(int size) { //получение элемента по индексу
-        return data[size];
+    public Object get(int elements) { //получение элемента по индексу
+        return myArray[elements];
     }
 
     @Override
     public String toString() {
-        return "(" + data.length + ")";
+        return "(" + myArray.length + ")";
     }
 
     public static void main(String[] args) {
