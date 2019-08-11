3efa8f16da4581ef254135041e67b56cd6ab055d
Vlad Sharachev <vladislav_sharachev@epam.com>
2019-08-11 12:25:45 +0400

    lesson4 vers 1.1b

--- src/com/epam/vladislav_sharachev/java/lesson4/task2/ArrayMethods.java
+++ src/com/epam/vladislav_sharachev/java/lesson4/task2/ArrayMethods.java
@@ -1,32 +1,33 @@
-package com.epam.vladislav_sharachev.java.lesson4.task2.tryA;
+package com.epam.vladislav_sharachev.java.lesson4.task2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 
-public class TryNow<Integer> {
+public  class ArrayMethods<Integer> {
 
     public final ArrayList<java.lang.Integer> one;
-    public TryNow(ArrayList<java.lang.Integer> one) {
+
+    public ArrayMethods(ArrayList<java.lang.Integer> one) {
         this.one = one;
     }
 
     public void getAdd() {
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 1000000; i++) {
             boolean add = one.add(i);//добавляем числа в массив
-            System.out.print("[" + i + "] "); // вывести на экран последовательность чисел
         }
-        System.out.println();
-        System.out.println();
     }
+
     public void getMix() {
         Collections.shuffle(one); //перемешиваем числа в массиве
         for (int i = 0; i < 10; i++) {
-            System.out.print("[" + one.get(i) + "] "); //вывести на экран массив #2
+            System.out.print("[" + one.get(i) + "] "); //вывести на экран массив 10 первых чисел
         }
         System.out.println();
         System.out.println();
     }
+
     public void getUnique() {
         ArrayList<java.lang.Integer> myMilli = new ArrayList(Arrays.asList(one));
         /* создаем отдельный список
@@ -35,17 +36,21 @@ public class TryNow<Integer> {
         System.out.println();
         System.out.println();
     }
+
     public void getMin() {
         System.out.print(Collections.min(one)); //вывести на экран минимальный элемент массива
         System.out.println();
         System.out.println();
     }
+
     public void getParity() {
         one.removeIf(i -> i % 2 != 0); //удалить все нечетные числа
         System.out.println(one); // вывести на экран все четные числа
         System.out.println();
     }
+
     public void getPenult() {
-        System.out.print(Collections.min(one)+1); //вывести предпоследнее число
+        Collections.sort(one); // отсортировать массив
+            System.out.println(one.get(1)); //вывести на экран предпоследний элемент
+        }
     }
-}
--- src/com/epam/vladislav_sharachev/java/lesson4/task2/DynamicArray.java
+++ src/com/epam/vladislav_sharachev/java/lesson4/task2/DynamicArray.java
@@ -1,53 +1,21 @@
 package com.epam.vladislav_sharachev.java.lesson4.task2;
 
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.util.ArrayList;
+import java.lang.Integer;
 
 public class DynamicArray<E> {
 
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> myMillion = new ArrayList(Arrays.asList()); //создаем массив с 1 000 000 пустых строк
-        for (int i = 0; i < 10; i++) {
-            boolean add = myMillion.add(i);//добавляем числа в массив
-            System.out.print("[" + i + "] "); // вывести на экран последовательность чисел
-        }
-        System.out.println();
-        System.out.println();
+        ArrayList<Integer> myMillion = new ArrayList(); //создаем массив
+        ArrayMethods tryNow = new ArrayMethods(myMillion); //ссылка на конструктор с методами
 
-
-        Collections.shuffle(myMillion); //перемешиваем числа в массиве
-        for (int i = 0; i < 10; i++) {
-            System.out.print("[" + myMillion.get(i) + "] "); //вывести на экран массив #2
-        }
-        ArrayList<Integer> myMilli = new ArrayList(Arrays.asList(myMillion)); /* создаем отдельный список
-         для проверки уникальности каждого элемента массива */
-
-        System.out.println();
-        System.out.println();
-        System.out.print(Collections.disjoint(myMilli, myMillion)); //вывести на экран подтверждение уникальности
-
-        System.out.println();
-        System.out.println();
-        System.out.print(Collections.min(myMillion)); //вывести на экран минимальный элемент массива
-
-
-        myMillion.removeIf(i -> i % 2 != 0); //удалить все нечетные числа
-
-        System.out.println();
-        System.out.println();
-        System.out.println(myMillion); //вывести на экран все четные числа
-
-        System.out.println();
-        System.out.println();
-        {
-            if (myMillion.removeIf(i -> i % 2 != 0)) ;
-            {
-                System.out.print(Collections.min(myMillion)+1); // предпоследний по величине элемент
-            }
-        }
+        tryNow.getAdd(); // добавить числа в массив
+        tryNow.getMix(); //перемешать и показать что числа перемешаны (первые 10 элементов)
+        tryNow.getUnique(); //создание 2-го массива для сравнения с 1-м (проверка уникальности)
+        tryNow.getMin(); //вывести на экран минимальный элемент массива
+        tryNow.getParity(); //удалить все нечетные числа, вывести все четные числа
+        tryNow.getPenult();// отсортировать массив и показать предпоследний по величине элемент
     }
 }
