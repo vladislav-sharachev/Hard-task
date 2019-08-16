41a65e201e926e3f6b657ce5e188110e72d63763
Vlad Sharachev <vladislav_sharachev@epam.com>
2019-08-16 17:58:40 +0400

    les7

--- src/com/epam/vladislav_sharachev/java/lesson7/task1/Spices/PutSpice.java
+++
@@ -1,5 +0,0 @@
-package com.epam.vladislav_sharachev.java.lesson7.task1.Spices;
-
-public interface PutSpice {
-    public void putSpice();
-}
--- src/com/epam/vladislav_sharachev/java/lesson7/task1/Vegetables/Salad.java
+++
@@ -1,5 +0,0 @@
-package com.epam.vladislav_sharachev.java.lesson7.task1.Vegetables;
-
-public interface Salad {
-    public void salad(); //использование интерфеса
-}
