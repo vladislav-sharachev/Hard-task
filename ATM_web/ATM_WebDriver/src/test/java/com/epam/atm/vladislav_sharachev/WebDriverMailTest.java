64ac79ade4e70c2a537e2139ba18cf1826c938f8
Vlad Sharachev <vladislav_sharachev@epam.com>
2019-08-26 22:44:28 +0400

    selenium ver 1.0

--- ATM_web/ATM_WebDriver/src/test/java/com/epam/atm/vladislav_sharachev/WebDriverMailTest.java
+++ ATM_web/ATM_WebDriver/src/test/java/com/epam/atm/vladislav_sharachev/WebDriverMailTest.java
@@ -21,151 +21,147 @@ public class WebDriverMailTest {
 
     @BeforeClass
     public void startBrowser() {
-        System.setProperty("webdriver.chrome.driver", "d:\\_webdriver\\chromedriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "c:\\_webdriver\\chromedriver\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized"); // Maximize browser window via options, just an example
+        options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
 
-        driver.get("https://yandex.ru"); //открыть страницу
+        driver.get("https://yandex.ru");
 
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test(priority = 1)
-    public void loginToTheMail() throws InterruptedException {
+    public void loginToTheMail() {
 
-        WebElement yaLoginEnter = driver.findElement(By.xpath("//a[contains(@class,'enter')]")); //элемент "Войти"
-        yaLoginEnter.click();
+        WebElement enter = driver.findElement(By.xpath("//a[contains(@class,'enter')]")); //элемент "Войти"
+        enter.click();
 
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']")));
 
-        WebElement yaLoginIn = driver.findElement(By.xpath("//input[contains(@id,'login')]")); //ввести логин
-        yaLoginIn.sendKeys("com-epam-at"); //epamtest123456 - пароль
+        WebElement userName = driver.findElement(By.xpath("//input[contains(@id,'login')]")); //ввести логин
+        userName.sendKeys("com-epam-at"); //epamtest123456 - пароль
 
-        WebElement yaLoginEnter2 = driver.findElement(By.xpath("//button[@type='submit']"));
-        yaLoginEnter2.click();
+        WebElement loginSubmit = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginSubmit.click();
 
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='password']")));
 
-        WebElement yaPassword = driver.findElement(By.xpath("//input[@type='password']"));
-        yaPassword.sendKeys("epamtest123456");
+        WebElement password = driver.findElement(By.xpath("//input[@type='password']"));
+        password.sendKeys("epamtest123456");
 
-        WebElement yaLoginEnter3 = driver.findElement(By.xpath("//button[@type='submit']"));
-        yaLoginEnter3.click();
-
-        Thread.sleep(3000);
+        WebElement passwordSubmit = driver.findElement(By.xpath("//button[@type='submit']"));
+        passwordSubmit.click();
 
     }
 
     @Test(priority = 2)
     public void assertTheLogin() {
-        new WebDriverWait(driver, 1);
-        WebElement yaMailLoginTrue = driver.findElement(By.cssSelector("div.mail-User-Name"));
-        Assert.assertTrue(yaMailLoginTrue.isDisplayed(), "No required element on the page!");
-
-        System.out.println("LogIn was successfully done.");
+        new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.mail-User-Name")));
+        WebElement assertLogIn = driver.findElement(By.cssSelector("div.mail-User-Name"));
+        Assert.assertTrue(assertLogIn.isDisplayed(), "No required element on the page!");
     }
 
     @Test(priority = 3)
-    public void createANewMail() throws InterruptedException {
-        new WebDriverWait(driver, 1);
-        WebElement yaWriteButton = driver.findElement(By.cssSelector("a.mail-ComposeButton"));
-        yaWriteButton.click();
+    public void createANewMail() {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a.mail-ComposeButton")));
 
-        Thread.sleep(3000);
+        WebElement newLetter = driver.findElement(By.cssSelector("a.mail-ComposeButton"));
+        newLetter.click();
 
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.js-compose-field[name^='to']")));
 
-        WebElement yaFillAdress = driver.findElement(By.cssSelector("div.js-compose-field[name^='to']"));
-        WebElement yaSubject = driver.findElement(By.xpath("//input[contains(@class,'mail-Compose-Field')]"));
-        WebElement yaMessageBody = driver.findElement(By.cssSelector("div.cke_wysiwyg_div"));
+        WebElement fillAdress = driver.findElement(By.cssSelector("div.js-compose-field[name^='to']"));
+        WebElement subject = driver.findElement(By.xpath("//input[contains(@class,'mail-Compose-Field')]"));
+        WebElement messageBody = driver.findElement(By.cssSelector("div.cke_wysiwyg_div"));
 
-        yaFillAdress.sendKeys("com-epam-at@yandex.ru");
-        yaSubject.sendKeys("Selenium WebDriver AT");
-        yaMessageBody.sendKeys("Привет! Это сообщение написано с помощью Selenium WebDriver");
-        Thread.sleep(3000);
+        fillAdress.sendKeys("com-epam-at@yandex.ru");
+        subject.sendKeys("Selenium WebDriver AT");
+        messageBody.sendKeys("Привет! Это сообщение написано с помощью Selenium WebDriver." +
+                " После написания ждем еще 3 сек - пока Яндекс сохранит черновик :)");
     }
 
     @Test(priority = 4)
     public void saveMail() {
-        new WebDriverWait(driver, 1);
-        WebElement yaChoiceDraft = driver.findElement(By.xpath("//span[text()='как черновик']"));
-        yaChoiceDraft.click();
+        new WebDriverWait(driver, 10).
+                until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='как черновик']")));
+        WebElement selectDraft = driver.findElement(By.xpath("//span[text()='как черновик']"));
+        selectDraft.click();
     }
 
     @Test(priority = 5)
-    public void verifyDraft() throws InterruptedException {
-        new WebDriverWait(driver, 1);
-        Thread.sleep(3000); //тот случай когда всеравно ждать 3 сек =)
+    public void verifyDraft() {
+        new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(@class,'mail') " +
+                        "and text()='Черновики']")));
 
-        WebElement yaSelectDraft = driver.findElement(By.xpath("//span[contains(@class,'mail') and text()='Черновики']"));
-        yaSelectDraft.click();
-        Thread.sleep(3000);
+        WebElement selectDraft = driver.findElement(By.xpath("//span[contains(@class,'mail') and text()='Черновики']"));
+        selectDraft.click();
     }
 
     @Test(priority = 6)
     public void verifyDraftContent() {
-        new WebDriverWait(driver, 1);
-        WebElement yaSelectDraftMail = driver.findElement(By.xpath("//span[text()='Это сообщение написано с помощью Selenium WebDriver']"));
-        yaSelectDraftMail.click();
+        new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'Это сообщение')]")));
 
-        WebElement yaVerifyTo = driver.findElement(By.xpath("//span[contains(@data-yabble-name,'com-epam-at')]"));
-        Assert.assertTrue(yaVerifyTo.isDisplayed(), "No required element on the page!");
+        WebElement selectDraftLetter = driver.findElement(By.xpath("//span[contains(text()," +
+                "'Это сообщение написано')]"));
+        selectDraftLetter.click();
 
-        WebElement yaVerifySubject = driver.findElement(By.xpath("//input[@value='Selenium WebDriver AT']"));
-        Assert.assertTrue(yaVerifySubject.isDisplayed(), "No required element on the page!");
+        WebElement verifyFillAdress = driver.findElement(By.xpath("//span[contains(@data-yabble-name," +
+                "'com-epam-at')]"));
+        Assert.assertTrue(verifyFillAdress.isDisplayed(), "No required element on the page!");
 
-        WebElement yaVerifyBody = driver.findElement(By.xpath("//div[contains(text(),'Привет! Это сообщение написано с помощью Selenium WebDriver')]"));
-        Assert.assertTrue(yaVerifyBody.isDisplayed(), "No required element on the page!");
+        WebElement verifySubject = driver.findElement(By.xpath("//input[@value='Selenium WebDriver AT']"));
+        Assert.assertTrue(verifySubject.isDisplayed(), "No required element on the page!");
+
+        WebElement verifyBody = driver.findElement(By.xpath("//div[contains(text(),'Привет! Это сообщение')]"));
+        Assert.assertTrue(verifyBody.isDisplayed(), "No required element on the page!");
 
     }
 
     @Test(priority = 7)
     public void sendTheMail() throws InterruptedException {
-        new WebDriverWait(driver, 1);
-        WebElement yaSendMessage = driver.findElement(By.xpath("//span[contains(text(),'Отправить')]"));
-        yaSendMessage.click();
+        WebElement sendMessage = driver.findElement(By.xpath("//span[contains(text(),'Отправить')]"));
+        sendMessage.click();
 
-        Thread.sleep(3000);
+        Thread.sleep(3000); //Тот случай когда нужно подождать 3 сек - пока яндекс обработает отправку письма
     }
 
     @Test(priority = 8)
-    public void verifyDraftAgain() throws InterruptedException {
-        new WebDriverWait(driver, 1);
-        WebElement yaSelectDraft2 = driver.findElement(By.xpath("//span[text()='Черновики']"));
-        yaSelectDraft2.click();
+    public void verifyDraftAgain() {
+        WebElement selectDraft = driver.findElement(By.xpath("//span[text()='Черновики']"));
+        selectDraft.click();
 
-        WebElement yaAssertDraft = driver.findElement(By.xpath("//div[contains(text(),'нет писем')]"));
-        Assert.assertTrue(yaAssertDraft.isDisplayed(), "No required element on the page!");
+        WebElement assertDraft = driver.findElement(By.xpath("//div[contains(text(),'нет писем')]"));
+        Assert.assertTrue(assertDraft.isDisplayed(), "No required element on the page!");
 
-        Thread.sleep(3000);
     }
 
     @Test(priority = 9)
-    public void verifySends() throws InterruptedException {
-        new WebDriverWait(driver, 1);
-        WebElement yaSelectSends = driver.findElement(By.xpath("//span[text()='Отправленные']"));
-        yaSelectSends.click();
+    public void verifySends() {
+        WebElement selectSent = driver.findElement(By.xpath("//span[text()='Отправленные']"));
+        selectSent.click();
 
-        WebElement yaVerifySends = driver.findElement(By.xpath("//span[contains(text(),'com-epam-at')]"));
-        Assert.assertTrue(yaVerifySends.isDisplayed(), "No required element on the page!");
+        WebElement verifySent = driver.findElement(By.xpath("//span[contains(text(),'com-epam-at')]"));
+        Assert.assertTrue(verifySent.isDisplayed(), "No required element on the page!");
 
-        Thread.sleep(3000);
     }
 
     @Test(priority = 10)
     public void logOff() {
-        new WebDriverWait(driver, 1);
-        WebElement yaLogOff = driver.findElement(By.xpath("//span[@id='recipient-1']"));
-        yaLogOff.click();
+        WebElement logOff = driver.findElement(By.xpath("//span[@id='recipient-1']"));
+        logOff.click();
 
-        WebElement yaLogOffSelect = driver.findElement(By.xpath("//a[contains(text(),'Выйти')]"));
-        yaLogOffSelect.click();
+        WebElement logOffSelect = driver.findElement(By.xpath("//a[contains(text(),'Выйти')]"));
+        logOffSelect.click();
     }
 
     @AfterClass //выполняется самым последним
     public void stopBrowser() {
         driver.quit();
-        System.out.println("All tests were successfully finished.");
     }
 }
